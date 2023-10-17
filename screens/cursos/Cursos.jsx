4ec6c69e02930d93684907940db098340222b67d
Victor Gabriel <vg.vg1@hotmail.com>
2023-10-16 21:45:51 -0300

    estilização de cursos, FAB, voltar ao add curso

--- screens/cursos/Cursos.jsx
+++ screens/cursos/Cursos.jsx
@@ -1,35 +1,50 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import React, { useEffect, useState } from 'react'
-import { Button, Text } from 'react-native-paper'
+import { Button, Card, FAB, IconButton, Text } from 'react-native-paper'
+import { useFocusEffect } from '@react-navigation/native';
+import { ScrollView } from 'react-native';
 
 const Cursos = ({ navigation }) => {
 
     const [cursos, setCursos] = useState([])
 
-    useEffect(() => {
-        AsyncStorage.getItem('cursos').then(resultado => {
-            
-            resultado = JSON.parse(resultado) || []
+    useFocusEffect(
+        React.useCallback(() => {
+            AsyncStorage.getItem('cursos').then(resultado => {
 
-            console.log(resultado)
-            setCursos(resultado)
-        })
-    }, [])
+                resultado = JSON.parse(resultado) || []
+
+                console.log(resultado)
+                setCursos(resultado)
+            })
+        }, [])
+    );
 
     return (
         <>
+            <ScrollView style={{ padding: 10 }}>
 
-            {cursos.map(item => (
-                <Text>{item.nome}</Text>
-            ))}
+                {cursos.map((item, i) => (
+                    <Card key={i} mode='outlined' style={{ marginBottom: 10 }}>
+                        <Card.Content >
+                            <Text variant="titleLarge">{item.nome}</Text>
+                            <Text variant="bodyMedium">Duração: {item.duracao} semestres</Text>
+                            <Text variant="bodyMedium">Modalidade: {item.modalidade}</Text>
+                        </Card.Content>
+                        <Card.Actions>
+                            <IconButton icon='pencil' />
+                            <IconButton icon='delete' />
+                        </Card.Actions>
+                    </Card>
+                ))}
 
-            <Button
-                icon='plus'
-                mode='contained'
+            </ScrollView>
+            <FAB
+                mode='elevated'
+                icon="plus"
+                style={{ position: 'absolute', right: 10, bottom: 10 }}
                 onPress={() => navigation.push('cursos-form')}
-            >
-                Novo
-            </Button>
+            />
         </>
     )
 }

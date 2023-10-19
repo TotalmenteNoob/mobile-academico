28e6987ff40133b0726a5dca0904aebccd44929d
Victor Gabriel <vg.vg1@hotmail.com>
2023-10-18 21:56:14 -0300

    função editar e excluir

--- screens/cursos/Cursos.jsx
+++ screens/cursos/Cursos.jsx
@@ -1,24 +1,46 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import React, { useEffect, useState } from 'react'
-import { Button, Card, FAB, IconButton, Text } from 'react-native-paper'
+import { Button, Card, FAB, IconButton, Text, Dialog, Portal } from 'react-native-paper'
 import { useFocusEffect } from '@react-navigation/native';
 import { ScrollView } from 'react-native';
 
 const Cursos = ({ navigation }) => {
-
+    
     const [cursos, setCursos] = useState([])
+    const [idExcluir, setIdExcluir] = useState(0)
+
+    const [visible, setVisible] = React.useState(false);
+    const showDialog = () => setVisible(true);
+    const hideDialog = () => setVisible(false);
+
 
     useFocusEffect(
         React.useCallback(() => {
             AsyncStorage.getItem('cursos').then(resultado => {
-
                 resultado = JSON.parse(resultado) || []
-
-                console.log(resultado)
                 setCursos(resultado)
             })
         }, [])
     );
+
+    function carregarDados() {
+        AsyncStorage.getItem('cursos').then(resultado => {
+            resultado = JSON.parse(resultado) || []
+            setCursos(resultado)
+        })
+    }
+
+    function confirmarExclusao(id) {
+        setIdExcluir(id)
+        setVisible(true)
+    }
+
+    function excluir() {
+        cursos.splice(idExcluir, 1)
+        AsyncStorage.setItem('cursos', JSON.stringify(cursos))
+        carregarDados()
+        setVisible(false)
+    }
 
     return (
         <>
@@ -32,11 +54,23 @@ const Cursos = ({ navigation }) => {
                             <Text variant="bodyMedium">Modalidade: {item.modalidade}</Text>
                         </Card.Content>
                         <Card.Actions>
-                            <IconButton icon='pencil' />
-                            <IconButton icon='delete' />
+                            <IconButton icon='pencil' iconColor='#009EFF' onPress={() => navigation.push('cursos-form', {id: i, curso: item})} />
+                            <IconButton icon='delete' iconColor='#E23F29' onPress={() => confirmarExclusao(i)} />
                         </Card.Actions>
                     </Card>
                 ))}
+
+                <Portal>
+                    <Dialog visible={visible} onDismiss={hideDialog}>
+                        <Dialog.Content>
+                            <Text variant="bodyLarge">Deseja realmente excluir?</Text>
+                        </Dialog.Content>
+                        <Dialog.Actions>
+                            <Button onPress={excluir}>Sim</Button>
+                            <Button onPress={hideDialog}>Não</Button>
+                        </Dialog.Actions>
+                    </Dialog>
+                </Portal>
 
             </ScrollView>
             <FAB

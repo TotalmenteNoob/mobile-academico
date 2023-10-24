7219087a6437b5a4b9e489780fed048177e1f8f9
Victor Gabriel <vg.vg1@hotmail.com>
2023-10-23 21:51:42 -0300

    validator e alteração de armazenamento com formik

--- screens/cursos/CursosForm.jsx
+++ screens/cursos/CursosForm.jsx
@@ -1,20 +1,22 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { Formik } from 'formik'
 import React, { useState } from 'react'
-import { ScrollView } from 'react-native'
+import { ScrollView, View } from 'react-native'
 import { Button, Text, TextInput } from 'react-native-paper'
+import * as Yup from 'yup';
 
 const CursosForm = ({ navigation, route }) => {
 
     const curso = route.params?.curso || {}
     const id = route.params?.id
 
-    const [dados, setDados] = useState(curso)
+    // const [dados, setDados] = useState(curso)
 
-    function handleChange(valor, campo) {
-        setDados({ ...dados, [campo]: valor })
-    }
+    // function handleChange(valor, campo) {
+    //     setDados({ ...dados, [campo]: valor })
+    // }
 
-    function salvar() {
+    function salvar(dados) {
 
         AsyncStorage.getItem('cursos').then(resultado => {
 
@@ -32,37 +34,73 @@ const CursosForm = ({ navigation, route }) => {
         })
     }
 
+    const cursoValidator = Yup.object().shape({
+        nome: Yup.string()
+            .min(5, `Valor muito curto`)
+            .max(10, `Valor muito grande`)
+            .required(`Campo obrigatório`),
+        duracao: Yup.number()
+            .min(1, `Valor muito curto`)
+            .max(10, `Valor muito grande`)
+            .required(`Campo obrigatório`),
+        modalidade: Yup.string()
+            .min(5, `Valor muito curto`)
+            .max(15, `Valor muito grande`)
+            .required(`Campo obrigatório`),
+    })
+
     return (
         <>
             <ScrollView style={{ margin: 15 }}>
                 <Text>Formulário de Cursos</Text>
 
-                <TextInput
-                    style={{ margin: 10 }}
-                    mode='outlined'
-                    label='Nome'
-                    value={dados.nome}
-                    onChangeText={(valor) => handleChange(valor, 'nome')}
-                />
+                <Formik
+                    initialValues={curso}
+                    validationSchema={cursoValidator}
+                    onSubmit={values => salvar(values)}
+                >
+                    {({ values, handleChange, handleSubmit, errors, touched }) => (
+                        <View>
+                            <TextInput
+                                style={{ margin: 10 }}
+                                mode='outlined'
+                                label='Nome'
+                                value={values.nome}
+                                onChangeText={handleChange('nome')}
+                            />
+                            {(errors.nome && touched.nome) &&
+                                <Text style={{ color: 'red', marginTop: 3 }}>{errors.nome}</Text>
+                            }
+                            <TextInput
+                                style={{ margin: 10 }}
+                                mode='outlined'
+                                label='Duração'
+                                keyboardType='decimal-pad'
+                                value={values.duracao}
+                                onChangeText={handleChange('duracao')}
+                            />
+                            {(errors.duracao && touched.duracao) &&
+                                <Text style={{ color: 'red', marginTop: 3 }}>{errors.duracao}</Text>
+                            }
+                            <TextInput
+                                style={{ margin: 10 }}
+                                mode='outlined'
+                                label='Modalidade'
+                                value={values.modalidade}
+                                onChangeText={handleChange('modalidade')}
+                            />
+                            {(errors.modalidade && touched.modalidade) &&
+                                <Text style={{ color: 'red', marginTop: 3 }}>{errors.modalidade}</Text>
+                            }
+                            <Button onPress={handleSubmit}>Salvar</Button>
 
-                <TextInput
-                    style={{ margin: 10 }}
-                    mode='outlined'
-                    label='Duração'
-                    keyboardType='decimal-pad'
-                    value={dados.duracao}
-                    onChangeText={(valor) => handleChange(valor, 'duracao')}
-                />
+                        </View>
+                    )}
 
-                <TextInput
-                    style={{ margin: 10 }}
-                    mode='outlined'
-                    label='Modalidade'
-                    value={dados.modalidade}
-                    onChangeText={(valor) => handleChange(valor, 'modalidade')}
-                />
+                </Formik>
 
-                <Button onPress={salvar}>Salvar</Button>
+
+
 
             </ScrollView>
         </>
